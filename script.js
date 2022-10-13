f47277a476e90445999213068547dd705bbb6651
David J Crockett <crockett.davidj@gmail.com>
2022-10-13 16:24:39 -0700

    Created Div's in HTML and linked them in JS using document.querySelector

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 var apiKey = config.MY_KEY;
+
+const notificationEl = document.querySelector("#notification");
+const tempEl = document.querySelector("#temp-value p");
+const descriptionEl = document.querySelector("#temp-description p");
+const locationEl = document.querySelector("#location p");
